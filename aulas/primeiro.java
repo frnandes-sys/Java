a363260ddfc2d80ce7c77eca4fc04681e33036c3
Daniel Fernandes <frnandes@icloud.com>
2026-08-04 17:11:54 -0300

    new

---
+++ aulas/primeiro.java
@@ -0,0 +1,15 @@
+public class primeiro {
+    public static void main(String[] args) {
+        String nome = "Daniel";
+        int idade = 18;
+        String email = "777ferna@gmail.com";
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade:" + idade);
+        System.out.println("Email:" + email);
+       
+
+
+
+    }
+
+}
